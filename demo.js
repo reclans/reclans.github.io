09ca39c5a81d1ecf2d7abefbc15aed1d6916e354
Noel Recla <31311562+reclans@users.noreply.github.com>
2021-10-21 15:32:28 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,3 @@
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyGCIEgL1QgLoNvc'}).base('appt2JEiD72oVFwyG');
-
 $().ready(function() {
    $("#text").html("testytest"]);
 });
