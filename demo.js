05311f72aed86094e0c79ab167b6de599a745364
Noel Recla <31311562+reclans@users.noreply.github.com>
2021-10-21 15:41:50 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,3 +1,6 @@
-$().ready(function() {
-   $("#text").html("testytest2");
+$().ready(function(){
+    $.getJSON( "https://api.airtable.com/v0/appt2JEiD72oVFwyG/project%20test?api_key=keyGCIEgL1QgLoNvc", function( data ) {
+    console.log(data);
+    $("#text").html(data["records"]);
+  });
 });
