57c8c898ba03e2029d2bfa32c4fc8db9309a5163
Noel Recla <31311562+reclans@users.noreply.github.com>
2021-10-26 08:18:30 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,11 +1,13 @@
 $().ready(function(){
     $.getJSON( "https://api.airtable.com/v0/appt2JEiD72oVFwyG/project%20test?api_key=keyGCIEgL1QgLoNvc", function( data ) {
-    console.log(typeof(data));
-    var test1 = JSON.stringify(data, null, 1);
-    console.log(typeof(test1));
-    console.log(JSON.stringify(data));
-    console.log(JSON.stringify(data, null, 1));
-    console.log(JSON.stringify(data, null, 2));            
-    $("#text").html(data);
+        var record = data.record[0];
+        console.log(record);
+        //console.log(typeof(data));
+        //var test1 = JSON.stringify(data, null, 1);
+        //console.log(typeof(test1));
+        //console.log(JSON.stringify(data));
+        //console.log(JSON.stringify(data, null, 1));
+        //console.log(JSON.stringify(data, null, 2));            
+        //$("#text").html(data);
   });
 });
